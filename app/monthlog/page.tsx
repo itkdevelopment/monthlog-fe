38f892a439801d7075fc02c031f6892e2f476808
devops-mwn <devops@mwnetworks.co.kr>
2025-08-05 10:44:49 +0900

    style: update readme

---
+++ app/monthlog/page.tsx
@@ -0,0 +1,10 @@
+import Sample from "@/components/monthlog/sample";
+
+export default function HomePage() {
+    return (
+        <div>
+            this is root directory of Monthlog development.
+            <Sample></Sample>
+        </div>
+    );
+}
