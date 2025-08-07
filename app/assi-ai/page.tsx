1c19bc9bc8ef808bffaa05d4efe053d197e25dc0
devops-mwn <devops@mwnetworks.co.kr>
2025-08-07 19:06:46 +0900

    fix: fix sample root

--- app/assi-ai/page.tsx
+++ app/assi-ai/page.tsx
@@ -1,9 +1,10 @@
+import Sample from "@/components/assi-ai/sample";
 
 export default function HomePage() {
     return (
         <div>
-            this is root directory of Monthlog development.
-            {/* <Sample></Sample> */}
+            this is root directory of AI assistant development.
+            <Sample></Sample>
         </div>
     );
 }
