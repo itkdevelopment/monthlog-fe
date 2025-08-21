1a61629d5e986576d53284186c5157a4c9cc0612
nomadc-monthler <nomadc.monthler@gmail.com>
2025-08-21 15:31:39 +0900

    [add] image domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["dev-files.monthler.kr"], // 허용할 이미지 도메인들 추가
+  },
 };
 
 export default nextConfig;
